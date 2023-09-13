5d5fdc2f602d344fa52efeb37751f5f4959dc233
searchsea123 <144882353+searchsea123@users.noreply.github.com>
2023-09-13 16:30:51 +0900

    Update common.js

--- common.js
+++ common.js
@@ -6,7 +6,7 @@ const chatbotToggler = document.querySelector('.chatbot-toggler');
 const chatbotCloseBtn = document.querySelector('.close-btn');
 
 let userMessage;
-const API_KEY = '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'; // api key 생성 후 링크 복사
+const API_KEY = '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'; // api key 생성 후 링크 복사
 
 const createChatLi = (message,className) => {
     const chatLi = document.createElement('li');
